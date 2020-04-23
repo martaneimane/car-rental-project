b60522c02a5dcc57f55809971899dee9ed52ba01
BadCrowfather <rvitenbergs@gmail.com>
2020-04-23 16:47:33 +0300

    adding Rental_Office Files

---
+++ src/main/java/com/example/repository/Rental_OfficeRepository.java
@@ -0,0 +1,7 @@
+package com.example.repository;
+
+import com.example.model.Rental_Office;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Rental_OfficeRepository extends JpaRepository<Rental_Office, Long> {
+}
