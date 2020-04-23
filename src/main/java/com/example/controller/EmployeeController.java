2e7bf130c8f12513edce3c7160ebed24c3fcd46f
martaneimane <martaneimane@outlook.com>
2020-04-23 22:08:55 +0300

    EmployeeController, EmployeeService updated

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -1,11 +1,36 @@
 package com.example.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.dto.EmployeeDTO;
+import com.example.service.EmployeeService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/rest/Employee")
 public class EmployeeController {
 
+    private final EmployeeService employeeService;
 
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/employees")
+    public List<EmployeeDTO> getAllEmployees(){
+        return employeeService.getAllEmployees();
+    }
+    @GetMapping("/employees({id})")
+    public EmployeeDTO getEmployeeById(@PathVariable Long id){
+        return employeeService.getEmployeeById(id);
+    }
+    @PostMapping("/employee")
+    public void createEmployee(@RequestBody EmployeeDTO employeeDTO){
+        employeeService.createEmployee(employeeDTO);
+    }
+
+    @PutMapping("/employee")
+    public void updateEmployee(@RequestBody EmployeeDTO employeeDTO){
+        employeeService.updateEmployee(employeeDTO);
+    }
 }
--- src/main/java/com/example/service/EmployeeService.java
+++ src/main/java/com/example/service/EmployeeService.java
@@ -1,8 +1,45 @@
 package com.example.service;
 
+import com.example.dto.EmployeeDTO;
+import com.example.dto.mapper.EmployeeMapper;
+import com.example.model.Employee;
+import com.example.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class EmployeeService {
 
+    private final EmployeeRepository employeeRepository;
+    private final EmployeeMapper employeeMapper;
+
+    @Autowired
+    public EmployeeService(EmployeeRepository employeeRepository, EmployeeMapper employeeMapper) {
+        this.employeeRepository = employeeRepository;
+        this.employeeMapper = employeeMapper;
+    }
+
+    public List<EmployeeDTO> getAllEmployees(){
+        return employeeRepository.findAll().stream()
+                .map(employeeMapper::employeeToDTO)
+                .collect(Collectors.toList());
+    }
+
+   public void createEmployee(EmployeeDTO employeeDTO){
+       Employee employee = employeeMapper.fromDto(employeeDTO);
+       employeeRepository.save(employee);
+   }
+
+   public void updateEmployee(EmployeeDTO employeeDTO){
+        Employee employee = employeeMapper.fromDto(employeeDTO);
+        employeeRepository.save(employee);
+   }
+
+   public EmployeeDTO getEmployeeById(Long id){
+        Employee employee = employeeRepository.getOne(id);
+        return employeeMapper.employeeToDTO(employee);
+   }
 }
